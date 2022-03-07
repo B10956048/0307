b026462b9739dc17668d1052cd84a619481d517c
張景陽 <@LAPTOP-RE47D272>
2022-03-07 21:06:14 +0800

    total

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,2 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+double meal_cost = 100;
+int tip_percent = 15;
+int tax_percent = 8;
+Console.WriteLine("1.總共:" + (meal_cost + tip_percent + tax_percent));
